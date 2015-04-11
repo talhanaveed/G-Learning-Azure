6b72de81eea631f38dfeb1dee8ef6781e24bffd3
unknown <talhanaveed123@gmail.com>
2015-04-11 11:35:13 +0500

    Mid Eval

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/G-Learning/';
+$config['base_url']	= 'http://glearning-test.azurewebsites.net/';
 /*
 |--------------------------------------------------------------------------
 | Index File
