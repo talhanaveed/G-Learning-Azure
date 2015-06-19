fd1f7a97470fd239660e04af7ab8f80ccee65693
unknown <b-natal@PC01RW10-PC.middleeast.corp.microsoft.com>
2015-06-19 20:14:35 +0500

    Integrated_19_6_15_v3

--- application/config/config.php
+++ application/config/config.php
@@ -15,7 +15,7 @@
 |
 */
 
-$config['base_url']	= 'http://localhost:81/G-Learning';
+$config['base_url']	= 'http://glearning-test.azurewebsites.net/';
 
 /*
 |--------------------------------------------------------------------------
